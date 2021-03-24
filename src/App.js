86664fb97179dc363c744784cb74a69d11da79b7
sk2ller <kingofgame.85819@gmail.com>
2021-03-24 22:43:27 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieList from './MovieList';
+
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor:"gray"}}>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
